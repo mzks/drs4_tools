cad1959b991a9dae2e03ac6dd3bced9d1d76582e
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2022-03-02 01:36:08 +0900

    Add scaler values

--- decoder/convert.C
+++ decoder/convert.C
@@ -14,8 +14,10 @@ int convert(TString filename = "sub0000.0000", TString dirname = "/Users/mzks/da
 	ULong64_t timestamp;
 	UInt_t timestamp_usec;
 	Float_t t1, u1, t2, u2, t3, u3, t4, u4;
+    UInt_t s0, s1, s2, s3, s4, s5;
 	Float_t wf[4][1024];
 	Float_t tbin[4][1024];
+    UInt_t scaler[6];
 
 	auto out_file = TFile::Open("./"+filename+".root", "RECREATE");
 	auto tree = new TTree("tree", "tree");
@@ -24,9 +26,17 @@ int convert(TString filename = "sub0000.0000", TString dirname = "/Users/mzks/da
 	tree->Branch("timestamp_usec", &timestamp_usec, "timestamp_usec/i");
 	tree->Branch("wf", wf, "wf[4][1024]/F");
 	tree->Branch("tbin", tbin, "tbin[4][1024]/F");
+	tree->Branch("scaler", scaler, "scaler[6]/i");
 
 
 	while(ifs >> eventid >> timestamp >> timestamp_usec){
+            ifs >> s0 >> s1 >> s2 >> s3 >> s4 >> s5;
+            scaler[0] = s0;
+            scaler[1] = s1;
+            scaler[2] = s2;
+            scaler[3] = s3;
+            scaler[4] = s4;
+            scaler[5] = s5;
 		for(int ibin=0;ibin<1024;++ibin){
 			ifs >> t1 >> u1 >> t2 >> u2 >> t3 >> u3 >> t4 >> u4;
 			tbin[0][ibin] = t1;
